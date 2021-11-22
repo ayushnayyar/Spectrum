d1616efedf5213dbb135d445b2812ced9f690a4a
ayushnayyar <ayushnayyar48@gmail.com>
2021-11-22 21:38:35 +0530

    - Added empty models, and protected passwords

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
+
+// Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECTION_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    () => console.log('Connected to DB')
+);
 
 app.listen(app.get('port'), () => {
-    console.log(
-        `🚀 Server running at http://localhost:${port} in %s mode`,
-        app.get('port'),
-        app.get('env')
-    );
+    console.log(`🚀 Server running at http://localhost:${port}`);
 });
