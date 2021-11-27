69ecd6a9ac2e427eee6647ab2815af7b0f37aa07
ayushnayyar <ayushnayyar48@gmail.com>
2021-11-27 14:58:23 +0530

    - Added update post route

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -23,3 +23,17 @@ export const createPost = async (req, res) => {
         res.status(409).json({ message: error.message });
     }
 };
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.types.ObjectId.isValid(_id))
+        return res.status(404).send('No posts with that id');
+
+    const updatedPost = await Post.findByIdAndUpdate(_id, post, {
+        new: true,
+    });
+
+    res.json(updatedPost);
+};
