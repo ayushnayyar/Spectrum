5223395a7a0c3fdcf77c2b4d1f41a748a343efb9
ayushnayyar <ayushnayyar48@gmail.com>
2022-01-05 16:16:17 +0530

    - Added auth check in App component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 import Routes from './routes';
 import './common/app.scss';
+import Login from './pages/login';
 
 function App() {
-  return (
-    <div className="App">
-      <Routes />
-    </div>
-  );
+  const user = JSON.parse(localStorage.getItem('profile'));
+
+  return <div className="App">{user ? <Routes /> : <Login />}</div>;
 }
 
 export default App;
